d7af9cc3885e80328b4794b1fdceca1245112dac
Macy Jiang <mjiang20@stuy.edu>
2020-06-22 10:14:15 -0400

    Initial Commit

---
+++ reminderAppTests/reminderAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  reminderAppTests.swift
+//  reminderAppTests
+//
+//  Created by Macy Jiang on 6/22/20.
+//  Copyright © 2020 Macy Jiang. All rights reserved.
+//
+
+import XCTest
+@testable import reminderApp
+
+class reminderAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
