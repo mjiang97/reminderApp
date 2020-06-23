0a2b8aaba6b5f72aad77bad5752ae425253388b2
Macy Jiang <mjiang20@stuy.edu>
2020-06-23 10:46:10 -0400

    initial commit

---
+++ reminderApp/toDo.swift
@@ -0,0 +1,14 @@
+//
+//  toDo.swift
+//  reminderApp
+//
+//  Created by Macy Jiang on 6/22/20.
+//  Copyright © 2020 Macy Jiang. All rights reserved.
+//
+
+import Foundation
+class toDo {
+    var name = ""
+    var important = false
+    
+}
---
+++ reminderApp/toDoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  toDoViewController.swift
+//  reminderApp
+//
+//  Created by Macy Jiang on 6/22/20.
+//  Copyright © 2020 Macy Jiang. All rights reserved.
+//
+
+import UIKit
+
+class toDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
